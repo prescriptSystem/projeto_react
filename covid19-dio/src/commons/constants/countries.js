04bfb054e4037a4ac3386c4970db052f5783168c
prescriptSystem <prescript.sac@gmail.com>
2022-05-09 20:17:54 -0300

    Projeto React Covid

---
+++ covid19-dio/src/commons/constants/countries.js
@@ -0,0 +1,53 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+import ChinaFlag from '../../assets/images/china.png'
+import GermanyFlag from '../../assets/images/germany.png'
+import ItalyFlag from '../../assets/images/italy.png'
+import UsaFlag from '../../assets/images/usa.png'
+import MexicoFlag from '../../assets/images/mexico.png'
+import CanadaFlag from '../../assets/images/canada.png'
+import JapanFlag from '../../assets/images/japan.png'
+
+const COUNTRIES = [
+  {
+    label: 'Alemanha',
+    value: 'germany',
+    flag: GermanyFlag
+  },
+  {
+    label: 'Brasil',
+    value: 'brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'China',
+    value: 'china',
+    flag: ChinaFlag
+  },
+  {
+    label: 'Estados Unidos',
+    value: 'usa',
+    flag: UsaFlag
+  },
+  {
+    label: 'Itália',
+    value: 'italy',
+    flag: ItalyFlag
+  },
+  {
+    label: 'México',
+    value: 'mexico',
+    flag: MexicoFlag
+  },
+  {
+    label: 'Canadá',
+    value: 'canada',
+    flag: CanadaFlag
+  },
+  {
+    label: 'Japão',
+    value: 'japan',
+    flag: JapanFlag
+  }
+]
+
+export default COUNTRIES
